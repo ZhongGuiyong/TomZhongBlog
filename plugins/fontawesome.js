c66edf1d7d66aeb31d048410d071eaa185ff10f2
Zhong GuiYong <814374557@qq.com>
2019-07-28 11:24:46 +0800

    完成博客主页的布局。

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
-import { faChartBar ,faCode, faKeyboard, faGamepad, faLanguage, faComments, faUser, faCalendarDay, faEye, faComment, faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons'
+import { faChartBar ,faCode, faKeyboard, faGamepad, faLanguage, faComments, faUser, faCalendarDay, faEye, faComment, faChevronLeft, faChevronRight, faSearch} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 config.autoAddCss = false
 
-library.add(faCode, faKeyboard, faChartBar, faGamepad, faLanguage, faComments, faUser, faCalendarDay, faEye, faComment, faChevronLeft, faChevronRight)
+library.add(faCode, faKeyboard, faChartBar, faGamepad, faLanguage, faComments, faUser, faCalendarDay, faEye, faComment, faChevronLeft, faChevronRight, faSearch)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
