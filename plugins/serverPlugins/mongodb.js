5679a7c8b6f5819e44c68a97029af424d748c1bf
ZhongGuiYong <zhongguiyong@chicv.com>
2019-12-09 14:53:28 +0800

    Add docker for blog.

--- plugins/serverPlugins/mongodb.js
+++ plugins/serverPlugins/mongodb.js
@@ -4,7 +4,7 @@ import initizeMongoData from './initizeMongoData'
 export default async () => {
   await mongoose.connect(
     // 'mongodb+srv://root:1234@cluster0-mtpng.mongodb.net/zguiyong?retryWrites=true&w=majority'
-    'mongodb://localhost:27017/zguiyong',
+    'mongodb://mongo/zguiyong',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true
