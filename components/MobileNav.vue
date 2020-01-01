d9760d64961401040dfe1b705905ce2ceb1be35e
ZhongGuiyong <814374557@qq.com>
2020-01-01 08:52:30 +0800

    Optimize the Mobile Nav.

--- components/MobileNav.vue
+++ components/MobileNav.vue
@@ -23,21 +23,21 @@
     <nav id="mobile-nav" :style="navBar">
       <ul class="nav-menu">
         <li>
-          <a href="index.html">Home</a>
+          <a href="/">首页</a>
         </li>
-        <li>
+        <!-- <li>
           <a href="about.html">About</a>
+        </li> -->
+        <li>
+          <a href="/article_home">个人感悟</a>
         </li>
         <li>
-          <a href="services.html">Services</a>
+          <a href="/dashboard">文章后台</a>
         </li>
-        <li>
-          <a href="portfolio.html">Portfolio</a>
-        </li>
-        <li>
+        <!-- <li>
           <a href="price.html">Pricing</a>
-        </li>
-        <li class="menu-has-children">
+        </li> -->
+        <!-- <li class="menu-has-children">
           <a href>Blog</a>
           <ul>
             <li>
@@ -69,7 +69,7 @@
         </li>
         <li>
           <a href="contact.html">Contact</a>
-        </li>
+        </li> -->
       </ul>
     </nav>
   </div>
