de545b099555750e7b81be74c7f23216ea939314
ZhongGuiyong <814374557@qq.com>
2020-01-01 14:58:23 +0800

    Setting the text of the about_banner.

--- components/About_Banner.vue
+++ components/About_Banner.vue
@@ -3,10 +3,13 @@
     <div class="container">
       <div class="row d-flex align-items center justify-content-center">
         <div class="about-content col-lg-12 text-center">
-          <h1 class="text-white">About me</h1>
+          <h1 class="text-white">心中所向，素履以往</h1>
+          <!-- <p class="text-white link-nav mt-10">
+            <a href="/">首页</a> →
+            <a href="/article_home">个人感悟</a>
+          </p> -->
           <p class="text-white link-nav mt-10">
-            <a href="/">Home</a> →
-            <a href="/about.html">About</a>
+            <a href="javascript:;">愿热爱都不曾被辜负</a>
           </p>
         </div>
       </div>
